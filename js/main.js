d0f911781d3cea86f3c673ca144bf5a087770501
DrTanis <49119029+DrTanis@users.noreply.github.com>
2020-10-08 01:14:20 +0300

    Исправил недочёты по линтеру

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
-'use strict';
+`use strict`;
 
 const NUMBER_OF_POSTS = 8;
-const CHECKIN = ['12:00', '13:00', '14:00'];
-const CHECKOUT = ['12:00', '13:00', '14:00'];
-const ROOM_TYPE = ['palace', 'flat', 'house', 'bungalo'];
-const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const CHECKIN = [`12:00`, `13:00`, `14:00`];
+const CHECKOUT = [`12:00`, `13:00`, `14:00`];
+const ROOM_TYPE = [`palace`, `flat`, `house`, `bungalo`];
+const FEATURES = [`wifi`, `dishwasher`, `parking`, `washer`, `elevator`, `conditioner`];
 const PHOTOS = [
-  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
-  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
-  'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
+  `http://o0.github.io/assets/images/tokyo/hotel1.jpg`,
+  `http://o0.github.io/assets/images/tokyo/hotel2.jpg`,
+  `http://o0.github.io/assets/images/tokyo/hotel3.jpg`,
 ];
 const LOCATIONS = {
   x: {
@@ -20,27 +20,27 @@ const LOCATIONS = {
     MAX: 630
   }
 };
-let arrayOfPosts =[];
+let arrayOfPosts = [];
 
-let mapPinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
-let mapPinsList = document.querySelector('.map__pins');
-let mapElement = document.querySelector('.map');
+let mapPinTemplate = document.querySelector(`#pin`).content.querySelector(`.map__pin`);
+let mapPinsList = document.querySelector(`.map__pins`);
+let mapElement = document.querySelector(`.map`);
 
-let showMap = function(){
-  mapElement.classList.remove('map--faded');
+let showMap = function () {
+  mapElement.classList.remove(`map--faded`);
 };
 
-let getRandomInt = function(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
-}
+// let getRandomInt = function(min, max) {
+//   min = Math.ceil(min);
+//   max = Math.floor(max);
+//   return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
+// };
 
-let getRandomIntInclusive = function(min, max) {
+let getRandomIntInclusive = function (min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
-}
+  return Math.floor(Math.random() * (max - min + 1)) + min; // Максимум и минимум включаются
+};
 
 let getRandomArrayElement = function (array) {
   return array[Math.floor(Math.random() * array.length)];
@@ -50,17 +50,17 @@ let getArrayRandomLength = function (array) {
   return array.slice(0, getRandomIntInclusive(1, array.length));
 };
 
-let createPostsArray = function(){
-  for( let i = 1; i <= NUMBER_OF_POSTS; i++){
+let createPostsArray = function () {
+  for (let i = 1; i <= NUMBER_OF_POSTS; i++) {
     let locationX = getRandomIntInclusive(LOCATIONS.x.MIN, LOCATIONS.x.MAX);
     let locationY = getRandomIntInclusive(LOCATIONS.y.MIN, LOCATIONS.y.MAX);
     arrayOfPosts[i - 1] = {
       author: {
-        avatar: 'img/avatars/user0' + i + '.png',
+        avatar: `img/avatars/user0` + i + `.png`,
       },
       offer: {
-        title: 'Lorem ipsum dolor sit.',
-        address: locationX + ', ' + locationY,
+        title: `Lorem ipsum dolor sit.`,
+        address: locationX + `, ` + locationY,
         price: getRandomIntInclusive(1000, 50000),
         type: getRandomArrayElement(ROOM_TYPE),
         rooms: getRandomIntInclusive(1, 3),
@@ -68,7 +68,7 @@ let createPostsArray = function(){
         checkin: getRandomArrayElement(CHECKIN),
         checkout: getRandomArrayElement(CHECKOUT),
         features: getArrayRandomLength(FEATURES),
-        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        description: `Lorem ipsum dolor sit amet consectetur, adipisicing elit.`,
         photos: getArrayRandomLength(PHOTOS),
       },
       location: {
@@ -78,29 +78,29 @@ let createPostsArray = function(){
     };
   }
   return arrayOfPosts;
-}
+};
 
-let createDomElem = function(objJS) {
+let createDomElem = function (objJS) {
   let arrayElement = mapPinTemplate.cloneNode(true);
-  let arrayImg = arrayElement.querySelector('img');
-  // let arrayImg = arrayElement.querySelector('button');
+  let arrayImg = arrayElement.querySelector(`img`);
+  // let arrayImg = arrayElement.querySelector(`button`);
   let coordX = objJS.location.x + arrayImg.width / 2;
   let coordY = objJS.location.y + arrayImg.height;
-  arrayElement.style = 'left: ' + coordX + 'px; top: ' + coordY + 'px;';
+  arrayElement.style = `left: ` + coordX + `px; top: ` + coordY + `px;`;
   arrayImg.src = objJS.author.avatar;
   arrayImg.alt = objJS.offer.title;
   return arrayElement;
-}
+};
 
-let createPosts = function(arrayOfData){
+let createPosts = function (arrayOfData) {
   let fragment = document.createDocumentFragment();
-  for (let i = 0; i < arrayOfData.length; i++){
+  for (let i = 0; i < arrayOfData.length; i++) {
     fragment.appendChild(createDomElem(arrayOfData[i]));
   }
   return fragment;
-}
+};
 
-var start = function () {
+let start = function () {
   showMap();
   mapPinsList.appendChild(createPosts(createPostsArray()));
 };
