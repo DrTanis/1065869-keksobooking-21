20033e9d6bec8ffcaa746d7cb63dd7eac9c25157
DrTanis <49119029+DrTanis@users.noreply.github.com>
2020-10-10 23:59:02 +0300

    Внёс правки от аставника по 3 заданию(моки)

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-`use strict`;
+"use strict";
 
 const NUMBER_OF_POSTS = 8;
-const CHECKIN = [`12:00`, `13:00`, `14:00`];
-const CHECKOUT = [`12:00`, `13:00`, `14:00`];
-const ROOM_TYPE = [`palace`, `flat`, `house`, `bungalo`];
+const CHECKIN_LIST = [`12:00`, `13:00`, `14:00`];
+const CHECKOUT_LIST = [`12:00`, `13:00`, `14:00`];
+const ROOM_TYPE_LIST = [`palace`, `flat`, `house`, `bungalo`];
 const FEATURES = [`wifi`, `dishwasher`, `parking`, `washer`, `elevator`, `conditioner`];
 const PHOTOS = [
   `http://o0.github.io/assets/images/tokyo/hotel1.jpg`,
@@ -20,40 +20,34 @@ const LOCATIONS = {
     MAX: 630
   }
 };
-let arrayOfPosts = [];
+const arrayOfPosts = [];
 
 let mapPinTemplate = document.querySelector(`#pin`).content.querySelector(`.map__pin`);
-let mapPinsList = document.querySelector(`.map__pins`);
-let mapElement = document.querySelector(`.map`);
+let mapPinElementList = document.querySelector(`.map__pins`);
+const mapElement = document.querySelector(`.map`);
 
-let showMap = function () {
+const showMap = function () {
   mapElement.classList.remove(`map--faded`);
 };
 
-// let getRandomInt = function(min, max) {
-//   min = Math.ceil(min);
-//   max = Math.floor(max);
-//   return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
-// };
-
-let getRandomIntInclusive = function (min, max) {
+const getRandomIntInclusive = function (min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min; // Максимум и минимум включаются
 };
 
-let getRandomArrayElement = function (array) {
+const getRandomArrayElement = function (array) {
   return array[Math.floor(Math.random() * array.length)];
 };
 
-let getArrayRandomLength = function (array) {
+const getArrayRandomLength = function (array) {
   return array.slice(0, getRandomIntInclusive(1, array.length));
 };
 
-let createPostsArray = function () {
+const createPostsArray = function () {
   for (let i = 1; i <= NUMBER_OF_POSTS; i++) {
-    let locationX = getRandomIntInclusive(LOCATIONS.x.MIN, LOCATIONS.x.MAX);
-    let locationY = getRandomIntInclusive(LOCATIONS.y.MIN, LOCATIONS.y.MAX);
+    const locationY = getRandomIntInclusive(LOCATIONS.y.MIN, LOCATIONS.y.MAX);
+    const locationX = getRandomIntInclusive(LOCATIONS.x.MIN, LOCATIONS.x.MAX);
     arrayOfPosts[i - 1] = {
       author: {
         avatar: `img/avatars/user0` + i + `.png`,
@@ -62,11 +56,11 @@ let createPostsArray = function () {
         title: `Lorem ipsum dolor sit.`,
         address: locationX + `, ` + locationY,
         price: getRandomIntInclusive(1000, 50000),
-        type: getRandomArrayElement(ROOM_TYPE),
+        type: getRandomArrayElement(ROOM_TYPE_LIST),
         rooms: getRandomIntInclusive(1, 3),
         guests: getRandomIntInclusive(2, 6),
-        checkin: getRandomArrayElement(CHECKIN),
-        checkout: getRandomArrayElement(CHECKOUT),
+        CHECKIN_LIST: getRandomArrayElement(CHECKIN_LIST),
+        checkout: getRandomArrayElement(CHECKOUT_LIST),
         features: getArrayRandomLength(FEATURES),
         description: `Lorem ipsum dolor sit amet consectetur, adipisicing elit.`,
         photos: getArrayRandomLength(PHOTOS),
@@ -80,29 +74,28 @@ let createPostsArray = function () {
   return arrayOfPosts;
 };
 
-let createDomElem = function (objJS) {
-  let arrayElement = mapPinTemplate.cloneNode(true);
-  let arrayImg = arrayElement.querySelector(`img`);
-  // let arrayImg = arrayElement.querySelector(`button`);
-  let coordX = objJS.location.x + arrayImg.width / 2;
-  let coordY = objJS.location.y + arrayImg.height;
+const createDomElem = function (objJS) {
+  const arrayElement = mapPinTemplate.cloneNode(true);
+  const arrayImg = arrayElement.querySelector(`img`);
+  const coordX = objJS.location.x + arrayImg.width / 2;
+  const coordY = objJS.location.y + arrayImg.height;
   arrayElement.style = `left: ` + coordX + `px; top: ` + coordY + `px;`;
   arrayImg.src = objJS.author.avatar;
   arrayImg.alt = objJS.offer.title;
   return arrayElement;
 };
 
-let createPosts = function (arrayOfData) {
-  let fragment = document.createDocumentFragment();
+const createPosts = function (arrayOfData) {
+  const fragment = document.createDocumentFragment();
   for (let i = 0; i < arrayOfData.length; i++) {
     fragment.appendChild(createDomElem(arrayOfData[i]));
   }
   return fragment;
 };
 
-let start = function () {
+const start = function () {
   showMap();
-  mapPinsList.appendChild(createPosts(createPostsArray()));
+  mapPinElementList.appendChild(createPosts(createPostsArray()));
 };
 
 start();
